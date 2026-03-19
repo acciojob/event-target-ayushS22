6cbfc0cb9ab96d01c55b6af0a01cafc613e5f7ae
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-03-19 12:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map(); // event -> Set of callbacks
   }
-  
+
+  addEventListener(event, callback) {
+    // If event not present, create a new Set
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+
+    // Add callback (Set prevents duplicates)
+    this.listeners.get(event).add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    // If event exists, remove the callback
+    if (this.listeners.has(event)) {
+      this.listeners.get(event).delete(callback);
+    }
+  }
+
+  dispatchEvent(event) {
+    // If no listeners, do nothing
+    if (!this.listeners.has(event)) return;
+
+    // Call all callbacks
+    for (let callback of this.listeners.get(event)) {
+      callback();
+    }
+  }
+}
